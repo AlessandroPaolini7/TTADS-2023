0cd429d3d9c9b8dd626dbc0e3bdd49095f1f38ef
Alessandro Paolini <alepaolinicaar@gmail.com>
2023-04-19 19:50:21 -0300

    Agrego footer básico

--- tp-ttads/src/Components/Footer/Footer.jsx
+++ tp-ttads/src/Components/Footer/Footer.jsx
@@ -1,11 +1,46 @@
 import React from 'react'
-import { FooterContainer } from './styles.js'
+import { FooterContainer,FooterLeft,FooterRight,FooterCenter } from './styles.js'
+import ShuffleIcon from '@mui/icons-material/Shuffle';
+import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
+import SkipNextIcon from '@mui/icons-material/SkipNext';
+import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
+import RepeatIcon from '@mui/icons-material/Repeat';
+import PlaylistPlayIcon from '@mui/icons-material/PlaylistPlay';
+import VolumeDownIcon from '@mui/icons-material/VolumeDown';
+import { Grid,Slider } from '@mui/material';
+
 const Footer = () => {
     return (
         <FooterContainer>
-            {/* <FooterLeft>left</FooterLeft>
-            <FooterCenter>center</FooterCenter>
-            <FooterRight>right</FooterRight> */}
+            <FooterLeft>
+                <img src="https://static.posters.cz/image/750/buque-costero/coldplay-parachutes-album-cover-i56853.jpg" alt="Album cover" />
+                <div>
+                    <h4>Trouble</h4>
+                    <p>Coldplay</p>
+                </div>
+                {/* La puta madre no puedo hacer que estas dos palabras queden más cerca */}
+            </FooterLeft>
+            <FooterCenter>
+                <ShuffleIcon/>
+                <SkipPreviousIcon/>
+                <PlayCircleOutlineIcon/>
+                <SkipNextIcon/>
+                <RepeatIcon/>
+
+            </FooterCenter>
+            <FooterRight>
+                <Grid container spacing={2}>
+                    <Grid item>
+                        <PlaylistPlayIcon/>
+                    </Grid>
+                    <Grid item>
+                        <VolumeDownIcon/>
+                    </Grid>
+                    <Grid item xs>
+                        <Slider aria-label="Volume" />
+                    </Grid>
+                </Grid>
+            </FooterRight>
         </FooterContainer>
     )
     }
